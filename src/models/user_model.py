27d1b0f84ebdf3cf3c9599d93885e3f9aa035b22
Die <dlibonati@leafnoise.io>
2026-03-23 11:27:24 -0300

    fix: remove migrations exclude in pre commit config and update requirements dev and remove deprecated pydantic v1 constr

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,6 +1,10 @@
-from pydantic import BaseModel, constr
+from typing import Annotated
+
+from pydantic import BaseModel, StringConstraints
+
+ConstrainedStr = Annotated[str, StringConstraints(min_length=1, strip_whitespace=True)]
 
 
 class UserModel(BaseModel):
-    username: constr(min_length=1, strip_whitespace=True)
-    password: constr(min_length=1, strip_whitespace=True)
+    username: ConstrainedStr
+    password: ConstrainedStr
